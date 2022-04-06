2ed6085bb72a1e1f160f13b344bee25f361d6732
CheesyGamer77 <43181046+CheesyGamer77@users.noreply.github.com>
2022-04-06 14:10:35 -0400

    Deactivate minimap and crosshair on pause

--- Assets/__Scripts/PauseHandler.cs
+++ Assets/__Scripts/PauseHandler.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PauseHandler : MonoBehaviour
 {
     public GameObject pausePanel;  // the pause menu panel that will be activated
+    public GameObject[] objectsToDeactivateOnPause;  // objects that should be inactive when paused, and active otherwise
 
     // Start is called before the first frame update
     void Start()
@@ -28,10 +29,31 @@ public class PauseHandler : MonoBehaviour
         bool isPaused = !pausePanel.activeSelf;
         
         if(isPaused)
-            Time.timeScale = 0f;
+            Pause();
         else
-            Time.timeScale = 1f; 
+            Resume();
 
         pausePanel.SetActive(isPaused);       
+    }
+
+    void Pause()
+    {
+        // deactivates all objects that should be deactivated when the game is paused, and sets the Time.timeScale to 0
+        SetGameObjectListActiveState(false);
+        Time.timeScale = 0f;
+    }
+
+    void Resume()
+    {
+        // reactivates any object that was deactivated by the pause menu, and sets the Time.timeScale back to normal (1)
+        SetGameObjectListActiveState(true);
+        Time.timeScale = 1f;
+    }
+
+    void SetGameObjectListActiveState(bool active)
+    {
+        foreach(GameObject gameObject in objectsToDeactivateOnPause) {
+            gameObject.SetActive(active);
+        }
     }
 }
